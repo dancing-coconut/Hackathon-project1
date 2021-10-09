d7fca0a73cccc4cec3141ae41dddac2f39143e01
Dancing Coconut <dancingcoconut12@gmail.com>
2021-10-09 19:37:26 +0530

    Sign Up Retry

--- src/Components/Landingpage/Signup.jsx
+++ src/Components/Landingpage/Signup.jsx
@@ -1,33 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function Signup(props) {
-    return (
-        <div className="form-content">
-            <form className="form">
-                <h1 className="form-title">Sign Up</h1>
-                <h3 className="form-tagline">Start your cryptocurrency journey with us today!</h3>
-                <div className="form-input">
-                    <input type="text" id="name" className="form-input"
-                        placeholder="Enter your name" />
-                </div>
-                <div className="form-input">
-                    <input type="email" id="email" className="form-input" 
-                        placeholder="Enter your e-mail" />
-                </div>
-                <div className="form-input">
-                    <input type="password" id="password" className="form-input" 
-                        placeholder="Enter your password" />
-                </div>
-                <div className="form-input">
-                    <input type="password" id="conf-password" className="form-input" 
-                        placeholder="Re-enter your password" />
-                </div>
-                <button className="register-button" type="submit">Sign Up</button>
-            </form>
+function SignUp(props){
+    return(
+        <div className="SignupBox" onClick={props.handleClose1}>
+            <div className="SignupContent">
+                <span  ></span>
+                {props.signup}
+            </div>
         </div>
     );
 }
-
-
-export default Signup;
-
+export default SignUp;
