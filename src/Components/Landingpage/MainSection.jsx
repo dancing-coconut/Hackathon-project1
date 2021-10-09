cbb1568c292d1d1edd4c4ff3eecef4ac815f60ba
Dancing Coconut <dancingcoconut12@gmail.com>
2021-10-09 18:47:39 +0530

    Sign Up page

--- src/Components/Landingpage/MainSection.jsx
+++ src/Components/Landingpage/MainSection.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Modal from 'react-modal';
+import { useState } from 'react';
+import Signup from "./Signup";
 
 function MainSection() {
+  const [modalIsOpen, setmodalIsOpen] = useState(false)
   return (
     <div className="MainContainer">
       <img className="mainsection__image" src="/Images/LandingPage/BackgroundImageMain.png" alt="Main" />
       <h1 className="title__main">Learn</h1>
       <p className="description__main">Cryptocurrency</p>
-      <Link to="/InfoPage">
-        <button type="button" className="button__main">
-          <p className="buttondesc__main">Get Started Now</p>
+      <button type="button" className="button__main" onClick={() => setmodalIsOpen(true)}>
+        <p className="buttondesc__main">Get Started Now</p>
+      </button>
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setmodalIsOpen(false)}>
+        <Signup />
+        <button onClick={() => setmodalIsOpen(false)}>
+          Close
         </button>
-      </Link>
+      </Modal>
     </div>
   );
 }
---
+++ src/Components/Landingpage/Signup.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Signup(props) {
+    return (
+        <div className="form-content">
+            <form className="form">
+                <h1 className="form-title">Sign Up</h1>
+                <h3 className="form-tagline">Start your cryptocurrency journey with us today!</h3>
+                <div className="form-input">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" id="name" className="form-input"
+                        placeholder="Enter your name" />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="email" className="form-label">E-mail</label>
+                    <input type="email" id="email" className="form-input" 
+                        placeholder="Enter your e-mail" />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" id="password" className="form-input" 
+                        placeholder="Enter your password" />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="conf-password" className="form-label">Confirm Password</label>
+                    <input type="password" id="conf-password" className="form-input" 
+                        placeholder="Re-enter your password" />
+                </div>
+                <button className="register-button" type="submit">Sign Up</button>
+                <span className="form-login">
+                    Already have an account? Login <a href="#">here</a>
+                </span>
+            </form>
+        </div>
+    );
+}
+
+
+export default Signup;
+
